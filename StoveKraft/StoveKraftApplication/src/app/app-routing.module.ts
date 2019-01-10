df0524363abeb0c3e2c34e4106bad129fdc8d8e8
rshaurya1 <shaurawat@deloitte.com>
2019-01-10 15:09:05 +0530

    Home Page added

--- StoveKraft/StoveKraftApplication/src/app/app-routing.module.ts
+++ StoveKraft/StoveKraftApplication/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'home', component: HomePageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
