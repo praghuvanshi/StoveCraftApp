7fcaad29221553c18321bd00fae3f65eabe1e9e3
rshaurya1 <shaurawat@deloitte.com>
2019-01-16 12:34:15 +0530

    Made styling consistent

--- StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/bachelors.component.ts
+++ StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/bachelors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bachelors',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BachelorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  moveToCookware() {
+    this.router.navigateByUrl('/kitchenset');
   }
 
 }
