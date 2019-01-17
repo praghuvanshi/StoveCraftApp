0dbc2e40c7e42cb833551413b469faded51afad3
praghuvanshi <42573742+praghuvanshi@users.noreply.github.com>
2019-01-17 12:37:13 +0530

    Css Fixes

--- StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/cook-weekends/cook-weekends.component.ts
+++ StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/cook-weekends/cook-weekends.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 import { Options } from 'ng5-slider';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cook-weekends',
@@ -10,7 +11,7 @@ import { Options } from 'ng5-slider';
 })
 export class CookWeekendsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   value: number = 100;
   options: Options = {
@@ -19,6 +20,10 @@ export class CookWeekendsComponent implements OnInit {
   };
 
   ngOnInit() {
+  }
+
+  moveToCookware() {
+    this.router.navigateByUrl('/kitchenset');
   }
   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 }
