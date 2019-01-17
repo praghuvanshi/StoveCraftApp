c08b44ded818281504cc47968ce5e58efcf494bd
praghuvanshi <42573742+praghuvanshi@users.noreply.github.com>
2019-01-17 13:35:37 +0530

    navigation added

--- StoveKraft/StoveKraftApplication/src/app/compare-products/compare-products.component.ts
+++ StoveKraft/StoveKraftApplication/src/app/compare-products/compare-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-compare-products',
   templateUrl: './compare-products.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  moveToCookware() {
+    this.router.navigateByUrl('/kitchenset');
   }
 
 }
