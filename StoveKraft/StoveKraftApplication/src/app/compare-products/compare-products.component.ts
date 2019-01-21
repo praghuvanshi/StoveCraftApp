de2c55be5eca07f58fef2699f22b6338fe1c0107
ATRAPA\snmisra <snmisra@deloitte.com>
2019-01-21 15:02:44 +0530

    routing fixes

--- StoveKraft/StoveKraftApplication/src/app/compare-products/compare-products.component.ts
+++ StoveKraft/StoveKraftApplication/src/app/compare-products/compare-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-compare-products',
   templateUrl: './compare-products.component.html',
@@ -7,12 +7,15 @@ import {Router} from '@angular/router';
 })
 export class CompareProductsComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
   moveToCookware() {
     this.router.navigateByUrl('/kitchenset');
+  }
+  moveToCart() {
+    this.router.navigateByUrl('/cart');
   }
 
 }
--- StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/cook-weekends/cook-weekends.component.ts
+++ StoveKraft/StoveKraftApplication/src/app/home-page/cook-ware/bachelors/cook-weekends/cook-weekends.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { Options } from 'ng5-slider';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cook-weekends',
@@ -24,6 +24,9 @@ export class CookWeekendsComponent implements OnInit {
 
   moveToCookware() {
     this.router.navigateByUrl('/kitchenset');
+  }
+  moveToCompare() {
+    this.router.navigateByUrl('/compareproducts');
   }
   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 }
